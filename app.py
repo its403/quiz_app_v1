fca706652182bc71de7119421b3abcc115e54f1e
Aviral Malik <aviralmalik.23@gmail.com>
2025-01-17 06:35:59 +0000

    Added routes file

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Quiz App"
+import config
+import routes
 
 if __name__ == "__main__":
     app.run(debug=True)
